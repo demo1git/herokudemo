37cdebe3eea50d13a49b4608b52e94e19729777d
Anand Joglekar <v-ajoglekar@expedia.com>
2018-08-09 14:52:52 +0530

    add server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist/herokudemo'));
+app.use(express.static('/dist/herokudemo'));
 app.get('/*', function(req, res){
-	res.sendFile(path.join(__dirname+'/dist/herokudemo/index.html'));
+	res.sendFile(path.join('/dist/herokudemo/index.html'));
 });
 // Start the app by listening on the default
 // Heroku port
